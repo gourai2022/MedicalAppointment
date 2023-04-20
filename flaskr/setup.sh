b09fba013b13078334144ab0567095a115cfc829
gourai2022 <gouraik@gmail.com>
2023-04-19 20:41:21 -0500

    update

--- flaskr/setup.sh
+++
@@ -1,7 +0,0 @@
-AUTH0_DOMAIN='dev-gys4qzsrt6aecaks.us.auth0.com'
-ALGORITHMS=['RS256']
-API_AUDIENCE='medapp'
-export DATABASE_URL="postgres:///medapp"
-export FLASK_APP=__init__.py
-export FLASK_DEBUG=True
-export FLASK_ENVIRONMENT=debug
